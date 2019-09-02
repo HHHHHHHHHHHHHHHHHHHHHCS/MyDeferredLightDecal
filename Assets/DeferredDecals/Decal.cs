2b489ef33ef27edf1dbbad4234c486738467c364
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-02 09:53:27 +0800

    temp

---
+++ Assets/DeferredDecals/Decal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Decal : MonoBehaviour
+{
+    public enum DecalType
+    {
+        DiffuseOnly,
+        NormalOnly,
+        Both,
+    }
+
+    public DecalType decalType;
+    public Material material;
+
+    private void OnEnable()
+    {
+    }
+
+    private void Start()
+    {
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    private void DrawGizmo(bool selected)
+    {
+        var col = new Color(0.0f, 0.7f, 1.0f, 1.0f);
+        col.a = selected ? 0.1f : 0.1f;
+        Gizmos.color = col;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(Vector3.zero,Vector3.one);
+        col.a = selected ? 0.5f : 0.2f;
+        Gizmos.color = col;
+        Gizmos.DrawWireCube(Vector3.zero,Vector3.one);
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawGizmo(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        DrawGizmo(true);
+    }
+}
---
+++ Assets/DeferredDecals/DeferredDecalSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeferredDecalSystem
+{
+    private static DeferredDecalSystem instance;
+
+    public static DeferredDecalSystem Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DeferredDecalSystem();
+            }
+
+            return instance;
+        }
+    }
+
+
+    private HashSet<Decal> decalDiffuse = new HashSet<Decal>();
+    private HashSet<Decal> decalNormals = new HashSet<Decal>();
+    private HashSet<Decal> decalBoth = new HashSet<Decal>();
+}
