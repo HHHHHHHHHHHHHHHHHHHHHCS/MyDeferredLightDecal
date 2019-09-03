225582089c5e5088022cc29d018dca8423ca3d59
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-03 10:00:15 +0800

    add

---
+++ Assets/DeferredDecals/DeferredDecalRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class DeferredDecalRenderer : MonoBehaviour
+{
+    public Mesh cubeMesh;
+
+    private Dictionary<Camera, CommandBuffer> cameras = new Dictionary<Camera, CommandBuffer>();
+
+    private void OnDisable()
+    {
+        foreach (var cam in cameras)
+        {
+            if (cam.Key)
+            {
+                cam.Key.RemoveCommandBuffer(CameraEvent.BeforeLighting, cam.Value);
+            }
+        }
+    }
+
+    private void OnWillRenderObject()
+    {
+        var act = gameObject.activeInHierarchy && enabled;
+        if (!act)
+        {
+            OnDisable();
+            return;
+        }
+
+        var cam = Camera.current;
+        if (!cam)
+            return;
+
+        CommandBuffer buf = null;
+        if (cameras.ContainsKey(cam))
+        {
+            buf = cameras[cam];
+            buf.Clear();
+        }
+        else
+        {
+            buf = new CommandBuffer();
+            buf.name = "Deferred Decals";
+            cameras[cam] = buf;
+
+
+            cam.AddCommandBuffer(CameraEvent.BeforeLighting, buf);
+        }
+    }
+}
--- Assets/DeferredDecals/DeferredDecalSystem.cs
+++ Assets/DeferredDecals/DeferredDecalSystem.cs
@@ -23,4 +23,28 @@ public class DeferredDecalSystem
     private HashSet<Decal> decalDiffuse = new HashSet<Decal>();
     private HashSet<Decal> decalNormals = new HashSet<Decal>();
     private HashSet<Decal> decalBoth = new HashSet<Decal>();
+
+    public void AddDecal(Decal d)
+    {
+        RemoveDecal(d);
+        if (d.decalType == Decal.DecalType.DiffuseOnly)
+        {
+            decalDiffuse.Add(d);
+        }
+        else if (d.decalType == Decal.DecalType.NormalOnly)
+        {
+            decalNormals.Add(d);
+        }
+        else if (d.decalType == Decal.DecalType.Both)
+        {
+            decalBoth.Add(d);
+        }
+    }
+
+    public void RemoveDecal(Decal d)
+    {
+        decalDiffuse.Remove(d);
+        decalNormals.Remove(d);
+        decalBoth.Remove(d);
+    }
 }
